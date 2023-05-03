c8a95915239fb9a904f2c5207e1616e0d23ddfd1
prajwaldhoble <prajwal.dhoble@neebal.com>
2023-05-03 16:59:51 +0530

    completed update product information page fully

--- ecommerce-project/src/app/seller-update-product/seller-update-product.component.ts
+++ ecommerce-project/src/app/seller-update-product/seller-update-product.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Route, Router } from '@angular/router';
 import { product } from '../datatype';
 import { ProductService } from '../services/product.service';
 @Component({
@@ -9,19 +9,32 @@ import { ProductService } from '../services/product.service';
   styleUrls: ['./seller-update-product.component.scss']
 })
 export class SellerUpdateProductComponent {
-  productData : undefined | product
-  constructor(private route: ActivatedRoute , private product : ProductService)  { }
+  productData: undefined | product
+  productMessage : undefined | string
+  constructor(private route: ActivatedRoute, private product: ProductService, private router : Router) { }
 
   ngOnInit(): void {
     let productId = this.route.snapshot.paramMap.get('id')
     console.warn(productId)
-    productId && this.product.getProduct(productId).subscribe((data) =>{
+    productId && this.product.getProduct(productId).subscribe((data) => {
       console.warn(data)
       this.productData = data
     })
   }
 
-  submit(data: any) {
-
+  submit(data: product) {
+    if(this.productData){
+      data.id =this.productData.id
+    }
+    this.product.updateProduct(data).subscribe((result) => {
+        if(result){
+          this.productMessage="Product Updated Successfully, redirecting...."
+        }
+    })
+    setTimeout(()=>{
+      this.productMessage = undefined
+      this.router.navigate(['seller-home'])
+    }, 3000)
+    
   }
 }
