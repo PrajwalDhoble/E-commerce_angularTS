5798b2a70aaff23a13d233f9d9fb655f48df7556
prajwaldhoble <prajwal.dhoble@neebal.com>
2023-05-03 12:41:55 +0530

    added update product page and routing and some css fixes

--- ecommerce-project/src/app/seller-home/seller-home.component.ts
+++ ecommerce-project/src/app/seller-home/seller-home.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import { of } from 'rxjs';
 import { product } from '../datatype';
 import { ProductService } from '../services/product.service';
-import { faTrash } from '@fortawesome/free-solid-svg-icons'
+import { faTrash , faEdit} from '@fortawesome/free-solid-svg-icons'
 @Component({
   selector: 'app-seller-home',
   templateUrl: './seller-home.component.html',
@@ -12,6 +12,7 @@ export class SellerHomeComponent {
   productList: undefined | product[]
   productMessage: undefined | string
   icon = faTrash
+  editIcon = faEdit
   constructor(private product: ProductService) { }
 
   ngOnInit(): void {
---
+++ ecommerce-project/src/app/seller-update-product/seller-update-product.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { OnInit } from '@angular/core';
+@Component({
+  selector: 'app-seller-update-product',
+  templateUrl: './seller-update-product.component.html',
+  styleUrls: ['./seller-update-product.component.scss']
+})
+export class SellerUpdateProductComponent {
+
+  submit(data: any) {
+
+  }
+}
