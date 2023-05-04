11c27faca43382b3e52e10b616f01357a952b112
prajwaldhoble <prajwal.dhoble@neebal.com>
2023-05-04 16:44:05 +0530

    trending page completed it is now dynamic

--- ecommerce-project/src/app/home/home.component.ts
+++ ecommerce-project/src/app/home/home.component.ts
@@ -9,13 +9,16 @@ import { ProductService } from '../services/product.service';
 })
 export class HomeComponent {
   popularProducts : undefined | product[]
-
+  trendyProducts : undefined | product[]
   constructor(private product:ProductService){}
 
   ngOnInit(): void{
     this.product.popularProducts().subscribe((data)=>{
       this.popularProducts = data
     });
+    this.product.trendyProducts().subscribe((data)=>{
+      this.trendyProducts = data
+    })
   }
 
 }
