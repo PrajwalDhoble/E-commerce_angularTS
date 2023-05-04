56c8c92197e9dff5bea0c462f790c3e791483861
prajwaldhoble <prajwal.dhoble@neebal.com>
2023-05-04 18:30:21 +0530

    made dynamic page for search component

--- ecommerce-project/src/app/search/search.component.ts
+++ ecommerce-project/src/app/search/search.component.ts
@@ -1,10 +1,21 @@
+import { query } from '@angular/animations';
 import { Component } from '@angular/core';
-
+import { ActivatedRoute } from '@angular/router';
+import { product } from '../datatype';
+import { ProductService } from '../services/product.service';
 @Component({
   selector: 'app-search',
   templateUrl: './search.component.html',
   styleUrls: ['./search.component.scss']
 })
 export class SearchComponent {
+  searchResult : undefined | product[]
+  constructor(private activateRoute:ActivatedRoute, private product : ProductService){}
 
+  ngOnInit(): void {
+    let query = this.activateRoute.snapshot.paramMap.get('query')
+    query && this.product.searchProducts(query).subscribe((result)=>{
+      this.searchResult = result
+    })
+  }
 }
